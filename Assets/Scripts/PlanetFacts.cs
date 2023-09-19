d7f1993c2cf756f0fd6901b15421ffdf20b95e2a
artdude47 <artcouser@gmail.com>
2023-09-19 13:07:45 -0400

    Adjusted scale of sim

---
+++ Assets/Scripts/PlanetFacts.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetFacts : MonoBehaviour
+{
+    public string planetName;
+    public float dayLength;
+    public float yearLength;
+    public float planetDiameter;
+    public float distanceFromSun;
+}
